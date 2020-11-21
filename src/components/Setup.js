d1c51bdb2ba65260d0ee1e8361af1a8c7d4010d5
Kevin Vandy <kevinvandy656@gmail.com>
2020-11-21 13:35:23 -0600

    fix compile

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { MonitorsArea } from './MonitorsArea';
 import { MonitorOptionsArea } from './MonitorOptionsArea';
-import { SettingsPanel } from './SettingsPanel';
 import { ButtonsArea } from './ButtonsArea';
 
 export const Setup = () => {
   return (
     <>
-      
       <ButtonsArea />
       <MonitorsArea />
       <MonitorOptionsArea />
