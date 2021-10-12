9266beb6b5af503b6bd5002127e5bfc06f09aa34
Kevin Vandy <kevinvandy656@gmail.com>
2021-10-11 22:19:40 -0500

    add netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    target: '#svelte'
+    target: '#svelte',
+    adapter: netlify()
   }
 };
 
