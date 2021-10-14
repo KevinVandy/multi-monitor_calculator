b2e2d2b05090515023da166f7eb802fd86d22e81
Kevin Vandy <kevinvandy656@gmail.com>
2021-10-13 22:21:11 -0500

    persist stores to local storage

--- src/components/Setup.svelte
+++ src/components/Setup.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+  import {
+    deskWidth,
+    getNewMonitor,
+    monitors,
+    scale
+  } from '../stores/SetupStore';
+  import { afterUpdate, onMount } from 'svelte';
   import CommandButtons from './CommandButtons.svelte';
   import Desk from './Desk.svelte';
   import MonitorOptionsArea from './MonitorOptionsArea.svelte';
+
+  onMount(() => {
+    monitors.set(
+      JSON.parse(localStorage.getItem('monitors')) ?? [getNewMonitor()]
+    );
+    deskWidth.set(JSON.parse(localStorage.getItem('deskWidth')) ?? 6);
+    scale.set(JSON.parse(localStorage.getItem('scale')) ?? 16);
+  });
+
+  afterUpdate(() => {
+    monitors.subscribe((ms) =>
+      localStorage.setItem('monitors', JSON.stringify(ms))
+    );
+    deskWidth.subscribe((dw) =>
+      localStorage.setItem('deskWidth', JSON.stringify(dw))
+    );
+    scale.subscribe((s) => localStorage.setItem('scale', JSON.stringify(s)));
+  });
 </script>
 
 <CommandButtons />
--- src/stores/SetupStore.ts
+++ src/stores/SetupStore.ts
@@ -1,12 +1,12 @@
-import type { IMonitor } from 'src/utils/interfaces';
+import type { IMonitor, ISetup } from 'src/utils/interfaces';
 import { writable } from 'svelte/store';
-import defaultMonitor from '../utils/defaultMonitor.json';
+import defaultSetup from '../utils/defaultSetup.json';
 
 export const getNewMonitor = (index = 0): IMonitor => ({
-  ...JSON.parse(JSON.stringify(defaultMonitor as IMonitor)),
+  ...JSON.parse(JSON.stringify(defaultSetup as ISetup)).monitors[0],
   index: index
 });
 
-export const scale = writable(16);
-export const deskWidth = writable(6);
-export const monitors = writable([getNewMonitor()]);
+export const scale = writable<number>(16);
+export const deskWidth = writable<number>(6);
+export const monitors = writable<IMonitor[]>([getNewMonitor()]);
