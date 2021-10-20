bb6a1d39a6805ae50f71ee9e09dd1a6f5e79681d
Kevin Vandy <kevinvandy656@gmail.com>
2021-10-19 21:56:16 -0500

    show spinner while reading from local storage

--- src/components/Setup.svelte
+++ src/components/Setup.svelte
@@ -6,10 +6,13 @@
     scale
   } from '../stores/SetupStore';
   import { afterUpdate, onMount } from 'svelte';
+  import CircularProgress from '@smui/circular-progress';
   import CommandButtons from './CommandButtons.svelte';
   import Desk from './Desk.svelte';
   import MonitorOptionsArea from './MonitorOptionsArea.svelte';
   import type { ISetup } from 'src/utils/interfaces';
+
+  let loading = true;
 
   onMount(() => {
     const storedSetup: ISetup | null = JSON.parse(
@@ -20,6 +23,7 @@
       deskWidth.set(storedSetup.deskWidth ?? 6);
       scale.set(storedSetup.scale ?? 16);
     }
+    loading = false;
   });
 
   afterUpdate(() =>
@@ -35,5 +39,9 @@
 </script>
 
 <CommandButtons />
-<Desk />
-<MonitorOptionsArea />
+{#if loading}
+  <CircularProgress style="height: 32px; width: 32px;" indeterminate />
+{:else}
+  <Desk />
+  <MonitorOptionsArea />
+{/if}
