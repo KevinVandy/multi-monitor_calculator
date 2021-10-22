4e4d40892c9b129f67c5bf8cec3ab84e1c3e2a0b
Kevin Vandy <kevinvandy656@gmail.com>
2021-10-21 20:09:50 -0500

    fixx css problems and introduce dark mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,15 @@
   import Setup from '../components/Setup.svelte';
 </script>
 
+<dark-mode-toggle
+  appearance="toggle"
+  dark="Dark"
+  light="Light"
+  permanent
+/>
+
 <h1>Mulit-Montitor Calculator</h1>
 <h2>A Tool For Planning Your Mulit-Montitor Setup!</h2>
 <main>
   <Setup />
 </main>
-
-<style>
-  main {
-    background-image: linear-gradient(#fff, #ccc);
-    min-height: calc(100vh - 100px);
-    padding-bottom: 100px;
-  }
-</style>
