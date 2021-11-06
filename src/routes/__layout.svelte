7e9177da56cee203d2bde9f47c1344cc201698db
Kevin Vandy <kevinvandy656@gmail.com>
2021-11-06 08:15:45 -0500

    add logos to top

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import { AppContent } from '@smui/drawer';
   import Navbar from '../components/Navbar.svelte';
+  import logo from '../images/favicon.png';
 </script>
 
 <Navbar />
 
 <AppContent class="app-content">
   <header>
+    <div>
+      <img alt="logo" src={logo} height="50" />
+      <img alt="logo" src={logo} height="50" />
+      <img alt="logo" src={logo} height="50" />
+    </div>
     <h1>Multi-Monitor Calculator</h1>
     <h2>A Tool For Planning Your Multi-Monitor Setup!</h2>
   </header>
@@ -28,6 +34,14 @@
     min-height: 120vh;
   }
 
+  div {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  img {
+    margin: 0 -0.4rem;
+  }
   @media (max-width: 600px) {
     header {
       padding-top: 5rem;
